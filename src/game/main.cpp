fe9fa0bb54eee88ba5ea80d50f1da9ce34f3ff59
benbrittain19302 <72400916+benbrittain19302@users.noreply.github.com>
2023-01-27 14:59:35 +0000

    minor main changes

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -4,14 +4,12 @@
 
 using namespace myengine;
 
-struct Enemy : Component
+struct Player : Component
 {
 	void onTick()
 	{
 		//std::cout << "Ticking" << std::endl;
-		m_angle += 0.25f;
-
-		getEntity()->getTransform()->setRotation(rend::vec3(0, m_angle, 0));
+		
 	}
 
 	void onDisplay()
@@ -29,32 +27,7 @@ struct CamController : Component
 
 	void onTick()
 	{
-		rend::vec3 newPos = getEntity()->getTransform()->getPosition();
-		if (m_input->cmd_w)
-		{
-			newPos.z -= 0.25f;
-		}
-		if (m_input->cmd_s)
-		{
-			newPos.z += 0.25f;
-		}
-		if (m_input->cmd_a)
-		{
-			newPos.x -= 0.25f;
-		}
-		if (m_input->cmd_d)
-		{
-			newPos.x += 0.25f;
-		}
-		if (m_input->cmd_space)
-		{
-			newPos.y += 0.25f;
-		}
-		if (m_input->cmd_lshift)
-		{
-			newPos.y -= 0.25f;
-		}
-		getEntity()->getTransform()->setPosition(newPos);
+		
 	}
 
 	void onDisplay()
@@ -64,17 +37,11 @@ struct CamController : Component
 
 	void onInit()
 	{
-		m_input = getEntity()->getCore()->getInput();
 
-		rend::vec3 startPos = rend::vec3(0, 2, 0);
-		rend::vec3 startRot = rend::vec3(-10, 0, 0);
-
-		getEntity()->getTransform()->setPosition(startPos);
-		getEntity()->getTransform()->setRotation(startRot);
 	}
 
 private:
-	std::shared_ptr<Input> m_input;
+	
 };
 
 int main(int argc, char *argv[])
@@ -82,7 +49,7 @@ int main(int argc, char *argv[])
 	std::shared_ptr<Core> core = Core::initialize();
 
 	std::shared_ptr<Entity> e = core->addEntity();
-	e->addComponent<Enemy>();
+	e->addComponent<Player>();
 	std::shared_ptr<TriangleRenderer> tr = e->addComponent<TriangleRenderer>();
 	tr->setMesh(core->getResources()->load<Model>("../src/assests/models/curuthers.obj"));
 	tr->setTexture(core->getResources()->load<ETexture>("../src/assests/models/textures/Whiskers_diffuse.png"));
@@ -105,6 +72,8 @@ int main(int argc, char *argv[])
 	std::shared_ptr<Entity> camE = core->addEntity();
 	camE->addComponent<Camera>();
 	camE->addComponent<CamController>();
+	camE->getTransform()->setPosition(rend::vec3(0, 3, 0));
+	camE->getTransform()->setRotation(rend::vec3(-20, 0, 0));
 
 	std::shared_ptr<BoxCollider> bc2 = camE->addComponent<BoxCollider>();
 	camE->addComponent<Rigidbody>();
